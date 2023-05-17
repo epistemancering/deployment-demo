25fe4e43b0f726ab89d5f50489ee5c8a4e5b10b3
Austin Henrie <nitsua15@gmail.com>
2023-05-17 13:58:40 -0600

    implemented axios

--- public/main.js
+++ public/main.js
@@ -5,6 +5,6 @@ const clickHandler = () => alert('The cat name is: Rascal')
 btn.addEventListener('click', clickHandler)
 otherBtn.addEventListener("click", function() {
     axios.post("/ma-post").then(function(response) {
-        alert(response)
+        alert(response.data)
     })
 })
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require("express")
 let api = express()
 api.use(express.static(__dirname + "/public"))
-AudioParamMap.post("/ma-post", function(request, response) {
+api.post("/ma-post", function(request, response) {
     response.send("ya po0sted!")
 })
 api.listen(4000, function() {
