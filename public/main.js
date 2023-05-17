c9c80e4d9c52d52626aeeea25d171445fbf23bf2
Austin Henrie <nitsua15@gmail.com>
2023-05-17 13:58:11 -0600

    implemented axios

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
 const btn = document.querySelector('button')
-
+let otherBtn = document.querySelector("#post")
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+otherBtn.addEventListener("click", function() {
+    axios.post("/ma-post").then(function(response) {
+        alert(response)
+    })
+})
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 let express = require("express")
 let api = express()
 api.use(express.static(__dirname + "/public"))
+AudioParamMap.post("/ma-post", function(request, response) {
+    response.send("ya po0sted!")
+})
 api.listen(4000, function() {
     console.log("up on 4000")
 })
